a32745fafa4a71a7cef95742d56e62e4422a6d9b
Cecilia Grace <cecigrace2003@gmail.com>
2026-06-15 14:24:17 +0300

    complete the coffee and book files

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,29 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title, page_count):
+        self.title = title
+        self.page_count = page_count
+        
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+    
+    @property   #setting the getter
+    def page_count(self):
+        return self._page_count
+    
+    @page_count.setter
+    def page_count(self, value):
+        if not isinstance(value, int):
+            print("page_count must be an integer")
+            self._page_count = None
+        else:
+            self._page_count = value
+            
+
+# book1 = Book("book1", "two")
+# print(f"{book1.page_count}")
+    
+        
     
         
